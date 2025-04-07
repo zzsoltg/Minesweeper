e613de77316339d003fc4489d93f2d7d2fe6054b
Zimmermann Zsolt Gábor <zimmermannzsoltgabor@gmail.com>
2025-04-07 23:33:40 +0200

    Függvényleírás módosítás

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -119,6 +119,7 @@ def display_uncovered_field(field, uncovered):
 def get_user_move(rows, cols):
     """
     Kéri a felhasználótól egy érvényes lépés megadását (sor, oszlop), amely a tábla határain belül van.
+    A flag parancs megadásával van lehetőség flagek megadására.
 
     A függvény kezeli a hibás bemeneteket, és biztosítja, hogy a megadott koordináták két egész szám,
     a megfelelő tartományban legyenek.
