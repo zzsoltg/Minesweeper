27749f4bcadd204d2adba44d674fdda5073340f6
Zimmermann Zsolt Gábor <zimmermannzsoltgabor@gmail.com>
2025-04-07 23:50:46 +0200

    Uncovered lista elemeinek módosítása a game_loop() függvényben

--- src/minesweeper.py
+++ src/minesweeper.py
@@ -214,7 +214,7 @@ def game_loop(numbered_field):
     rows = len(numbered_field)
     columns = len(numbered_field[0])
     # Létrehozunk egy rácsot, amely jelzi, mely cellák lettek felfedve (kezdetben minden hamis)
-    uncovered = [[False for _ in range(columns)] for _ in range(rows)]
+    uncovered = [["U" for _ in range(columns)] for _ in range(rows)]
 
     while True:
         display_uncovered_field(numbered_field, uncovered)
